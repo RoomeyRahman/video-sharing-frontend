76bdd87b0ecc6f930077762a599028a815a3e827
roomey_rahman <roomeyrahman@gmail.com>
2022-03-17 13:43:38 +0600

    update home block

---
+++ components/home/Video-block.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import dynamic from "next/dynamic";
+
+const VideoCard = dynamic(() => import("../common/VideoCard"));
+
+type IProps = {};
+type IState = {};
+
+const videos = [
+  {
+    thumbnail: "/mountain.jpg",
+    title: "Mountain",
+    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Voluptatibus quia, Nonea! Maiores et perferendis eaque,
+    exercitationem praesentium nihil.`,
+    tags: ['photography', 'nature'],
+  },
+  {
+    thumbnail: "/mountain.jpg",
+    title: "Mountain",
+    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Voluptatibus quia, Nonea! Maiores et perferendis eaque,
+    exercitationem praesentium nihil.`,
+    tags: ['photography', 'nature'],
+  },
+  {
+    thumbnail: "/mountain.jpg",
+    title: "Mountain",
+    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Voluptatibus quia, Nonea! Maiores et perferendis eaque,
+    exercitationem praesentium nihil.`,
+    tags: ['photography', 'nature'],
+  },
+];
+
+class VideoBlock extends React.Component<IProps, IState> {
+  render() {
+    return (
+      <div className="bg-white">
+        <div className="container mx-auto px-5 py-10">
+          {/* <h2 className="text-3xl text-center lg:mb-9">Recent Service</h2> */}
+          {videos &&
+            Array.isArray(videos) &&
+            videos.length > 0 &&
+            videos.map((video, index) => {
+              return (
+                <div key={index} className="md:w-1/2 lg:w-1/4 transform hover:scale-105 duration-300 transition-transform">
+                  <div className="p-1 w-full rounded-md">
+                    <VideoCard 
+                        thumbnail={video.thumbnail}
+                        title={video.title}
+                        description={video.description}
+                        tags={video.tags}
+                    />
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    );
+  }
+}
+export default VideoBlock;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,14 @@ import type { NextPage } from "next";
 import dynamic from "next/dynamic";
 
 const MasterLayout = dynamic(() => import("../components/layout/Layout"));
-const VideoCard = dynamic(() => import("../components/common/VideoCard"));
+const VideoBlock = dynamic(() => import("../components/home/Video-block"));
 
 const Home: NextPage = () => {
   const title = "Share your creativity with your story";
 
   return (
     <MasterLayout title={title}>
-      <VideoCard />
+      <VideoBlock />
     </MasterLayout>
   );
 };
