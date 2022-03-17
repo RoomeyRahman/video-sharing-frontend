68cf790bc619e46faa600dc3959aea3afa4e394e
roomey_rahman <roomeyrahman@gmail.com>
2022-03-17 13:10:34 +0600

    add video card component

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,14 @@ import styles from "../styles/Home.module.css";
 import dynamic from "next/dynamic";
 
 const MasterLayout = dynamic(() => import("../components/layout/Layout"));
+const VideoCard = dynamic(() => import("../components/common/VideoCard"));
 
 const Home: NextPage = () => {
   const title = "Share your creativity with your story";
 
   return (
     <MasterLayout title={title}>
-      
+      <VideoCard />
     </MasterLayout>
   );
 };
