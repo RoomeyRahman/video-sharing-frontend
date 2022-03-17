3b41755830baa80686143fcec59c2b6eee4f4c33
roomey_rahman <roomeyrahman@gmail.com>
2022-03-17 13:14:04 +0600

    add error boundary component

---
+++ components/common/Error-boundary.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+interface IState {
+  error: any;
+  errorInfo: any;
+  hasError: boolean;
+}
+
+export default class ErrorBoundary extends React.Component<{}, IState> {
+  constructor(props) {
+    super(props);
+    this.state = { error: null, errorInfo: null, hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { error: error, hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    // logErrorToMyService(error, errorInfo);
+    this.setState({
+      error: error,
+      errorInfo: errorInfo,
+      hasError: true,
+    });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h1>Something went wrong.</h1>
+          <details style={{ whiteSpace: "pre-wrap" }}>
+            {this.state.error && this.state.error.toString()}
+            <br />
+            {this.state.errorInfo.componentStack}
+          </details>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
