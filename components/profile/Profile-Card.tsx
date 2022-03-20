553c421fd9b22f0df4ae540b1fbfa4a039f37c5f
roomey_rahman <roomeyrahman@gmail.com>
2022-03-20 13:49:30 +0600

    update

--- components/profile/Profile-Card.tsx
+++ components/profile/Profile-Card.tsx
@@ -1,17 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
+import {
+  getUserRetrive,
+  postUserLoginRefresh,
+} from "../../services/user.service";
 
+const cookie = require("cookie");
 const Img = dynamic(() => import("../common/Image-loader"));
 
 type IProps = {};
 
 const ProfileCard: React.FunctionComponent<IProps> = (props: IProps) => {
+  const [me, setMe] = useState<any>({});
+  useEffect(() => {
+    setCurrentUser();
+  }, []);
+
+  const setCurrentUser = async () => {
+    const response: any = await getUserRetrive({});
+    if (response && response.status === 200) {
+      setMe(response.data);
+    } else if (response && response.status === 401) {
+      const isRefreshed = await refreshLogin();
+      if (isRefreshed == true) {
+        setCurrentUser();
+      } else {
+        // something went wrong
+      }
+    }
+  };
+
+  const refreshLogin = async () => {
+    const cookies = cookie.parse(document.cookie);
+    const refresh = cookies.refresh;
+    const response: any = await postUserLoginRefresh({
+      data: { refresh: refresh },
+    });
+    if (response && response.status === 200) {
+      const maxAge = 864000000;
+      await fetch("/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          token: response.data.access,
+          refresh: refresh,
+          tokenMaxAge: maxAge,
+        }),
+      });
+      return true;
+    } else {
+      return false;
+    }
+  };
+
   return (
     <div className="bg-white p-3 border-t-4 border-green-400">
       <div className="image overflow-hidden">
         <Img
           className="h-auto w-full mx-auto"
-          src="https://lavinephotography.com.au/wp-content/uploads/2017/01/PROFILE-Photography-112.jpg"
+          src={
+            "/pp.png" ||
+            "https://lavinephotography.com.au/wp-content/uploads/2017/01/PROFILE-Photography-112.jpg"
+          }
           alt=""
           width={600}
           height={600}
@@ -20,15 +72,15 @@ const ProfileCard: React.FunctionComponent<IProps> = (props: IProps) => {
         />
       </div>
       <h1 className="text-gray-900 font-bold text-xl leading-8 my-1">
-        Jane Doe
+        {me.username}
       </h1>
       <h3 className="text-gray-600 font-lg text-semibold leading-6">
-        Owner at Her Company Inc.
+        {me.email}
       </h3>
-      <p className="text-sm text-gray-500 hover:text-gray-600 leading-6">
+      {/* <p className="text-sm text-gray-500 hover:text-gray-600 leading-6">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit,
         eligendi dolorum sequi illum qui unde aspernatur non deserunt
-      </p>
+      </p> */}
       <ul className="bg-gray-100 text-gray-600 hover:text-gray-700 hover:shadow py-2 px-3 mt-3 divide-y rounded shadow-sm">
         <li className="flex items-center py-3">
           <span>Status</span>
@@ -38,10 +90,10 @@ const ProfileCard: React.FunctionComponent<IProps> = (props: IProps) => {
             </span>
           </span>
         </li>
-        <li className="flex items-center py-3">
+        {/* <li className="flex items-center py-3">
           <span>Member since</span>
           <span className="ml-auto">Nov 07, 2016</span>
-        </li>
+        </li> */}
       </ul>
     </div>
   );
